b4e2804769ea862e2b2cc009632e1801a422c191
Pranav Ajith <pranav.ajith.cse21@itbhu.ac.in>
2024-10-05 12:18:44 +0530

    Added urls

---
+++ frontend/src/urls.js
@@ -0,0 +1,34 @@
+const urlList = {
+  // backendDatabase: "https://nagrik-aur-samvidhan-2wpj.onrender.com",
+  backendDatabase: "http://localhost:8080",
+  LegislatureUrl:
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSR2ZjfKmLE4olLUlPn1e5CGhK8ADxQeOiS_A&s",
+  ExecutiveUrl:
+    "https://i.pinimg.com/originals/3a/f9/85/3af98539c7c6f98b629eaf46943bc761.jpg",
+  JudicaryUrl:
+    "https://images.thequint.com/thequint/2020-05/2179b199-eed8-4851-9571-b21aee09d148/Supreme_Court_Quint.jpg?auto=format,compress&fmt=webp&format=webp&w=1200&h=900&dpr=1.0",
+  PreambleUrl:
+    "https://legal-wires.com/wp-content/uploads/light-of-Preamble-1024x455.png",
+  LockUrl: "https://www.freeiconspng.com/thumbs/lock-icon/lock-icon-11.png",
+  OldBookCoverUrl:
+    "https://i.pinimg.com/736x/67/8d/db/678ddb1a34dd467450b59950131c7be7.jpg",
+  OldBookInsideUrl:
+    "https://img.freepik.com/free-vector/ripped-newspaper-flower-stem-old-brown-paper-banner-vector_53876-136540.jpg?size=626&ext=jpg&ga=GA1.1.2008272138.1725753600&semt=ais_hybrid",
+  WorkImageUrl: "https://cdn-icons-png.flaticon.com/512/2344/2344036.png",
+  GenericKidImageUrl:
+    "https://png.pngtree.com/background/20230528/original/pngtree-photo-of-a-girl-in-sunglasses-picture-image_2778708.jpg",
+  TrophyPicUrl:
+    "https://static.vecteezy.com/system/resources/previews/022/419/488/original/medal-isolated-on-a-transparent-background-free-png.png",
+  CheckpointUrl:
+    "https://i.pinimg.com/474x/3c/fe/54/3cfe546290941b7e420a052f6b36dad1.jpg",
+  TrophyGifUrl: "https://cdn-icons-mp4.flaticon.com/512/8617/8617218.mp4",
+  CheckpointGifUrl: "https://cdn-icons-mp4.flaticon.com/512/6844/6844338.mp4",
+  WhiteLockUrl:
+    "https://static.vecteezy.com/system/resources/previews/009/589/656/original/lock-icon-transparent-free-png.png",
+  StagePicUrl:
+    "https://img.freepik.com/premium-photo/conference-hall-business-event-empty-stage-empty-stage-with-screen-procto_386094-230.jpg?w=360",
+  ConstitutionMakersPic:
+    "https://s02.sgp1.cdn.digitaloceanspaces.com/article/114370-qwxkrlmtwv-1567065827.jpg",
+};
+
+export { urlList };
