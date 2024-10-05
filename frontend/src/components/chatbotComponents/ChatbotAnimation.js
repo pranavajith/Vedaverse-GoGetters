7ef74de91e08818b44b40c260dd231ab585a9303
Pranav Ajith <pranav.ajith.cse21@itbhu.ac.in>
2024-10-05 12:20:48 +0530

    Added chatbot

---
+++ frontend/src/components/chatbotComponents/ChatbotAnimation.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const ChatbotAnimation = () => {
+  return (
+    <dotlottie-player
+      className="books-fall"
+      src="https://lottie.host/eae66761-adf7-4474-be8c-766ead075a96/7FAiwoTJ7d.json"
+      background="transparent"
+      speed="1"
+      style={{ width: "300px", height: "300px" }}
+      loop
+      autoplay
+    ></dotlottie-player>
+  );
+};
+
+export default ChatbotAnimation;
